c9ae23f3631914c1c837dfb2b7a97062d64e5eea
Ximena <ximenasaenz201922@gmail.com>
2025-11-11 22:30:58 -0500

    Forma para mostar el error

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,1 +1,26 @@
 const Juego= requiere('../Gamemodels/');
+
+// Crear Juego
+exports.createGame = async (req, res) => {
+    try{
+        const nuevoJuego = new Juego(req.body);
+        await nuevoJuego.save();
+        res.status(201).json(nuevoJuego);   
+    } catch (error) {
+        res.status(400).json({ mensaje: 'Error al crear el juego', error });
+    }
+}
+
+export.crearJuego = async (req, res) => {
+    try {
+        const nuevoJuego = new Juego(req.body);
+        await nuevoJuego.save();
+        res.status(201).json(nuevoJuego);
+    } catch (error) {
+        req.status(400).json({ 
+            error: 'Error al crear el juego. Verfique los cambios', 
+            details: error. message 
+        })
+    
+    }
+}
