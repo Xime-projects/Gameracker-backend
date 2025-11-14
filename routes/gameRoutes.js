f3a0c948586c1e66b7132ef356bf77c5e4256df0
Ximena <ximenasaenz201922@gmail.com>
2025-11-14 11:19:09 -0500

    Arreglo del CRUD

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const GameController = require('../controllers/gameController');
 
 // Rutas CRUD para juegos
-router.post('/games', GameController.createGame);
-router.get('/games', GameController.getAllGames);
-router.get('/games/:id', GameController.getGameById);
-router.put('/games/:id', GameController.updateGameById);
-router.delete('/games/:id', GameController.deleteGameById);
+router.post('/', GameController.createGame);
+router.get('/', GameController.getAllGames);
+router.get('/:id', GameController.getGameById);
+router.put('/:id', GameController.updateGameById);
+router.delete('/:id', GameController.deleteGameById);
 
 module.exports = router;
