eeecbfd0eb0595fe9880e12ed98b171e1cd47ac9
Ximena <ximenasaenz201922@gmail.com>
2025-11-16 11:53:30 -0500

    Routes modificado para subir vrios juegos a la vez

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const GameController = require('../controllers/gameController');
+const { createGamesBulk } = require('../controllers/gamesController');
+
 
 // Rutas CRUD para juegos
 router.post('/', GameController.createGame);
@@ -8,5 +10,7 @@ router.get('/', GameController.getAllGames);
 router.get('/:id', GameController.getGameById);
 router.put('/:id', GameController.updateGameById);
 router.delete('/:id', GameController.deleteGameById);
+router.post('/bulk', createGamesBulk);
+
 
 module.exports = router;
