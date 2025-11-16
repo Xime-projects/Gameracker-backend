c348f32b6d30ccb992fba8efaa51c2faeb3b4958
Ximena <ximenasaenz201922@gmail.com>
2025-11-16 12:21:40 -0500

    Correcciones

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const GameController = require('../controllers/gameController');
-const { createGamesBulk } = require('../controllers/gamesController');
+const { createGamesBulk } = require('../controllers/gameController');
 
 
 // Rutas CRUD para juegos
@@ -10,7 +10,8 @@ router.get('/', GameController.getAllGames);
 router.get('/:id', GameController.getGameById);
 router.put('/:id', GameController.updateGameById);
 router.delete('/:id', GameController.deleteGameById);
-router.post('/bulk', createGamesBulk);
 
+// Crear varios juegos a la vez
+router.post('/bulk', GameController.createGamesBulk);
 
 module.exports = router;
